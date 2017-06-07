5b4e6a32800d9380ca0307ccfe5c62684109e2db
darthplagueisthewise <josheverly18@yahoo.com>
2017-06-07 15:45:28 -0500

    Update hashtablechain.cpp

--- hashtablechain.cpp
+++ hashtablechain.cpp
@@ -1,1 +1,22 @@
 
+template<ItemType type>
+class BucketList {
+
+  struct Node {
+    type stored_data;
+    Node* next; 
+    
+    Node(type d, Node* np) 
+    {
+      stored_data = d;
+      next = np;
+    }
+  };
+  
+  
+  private:
+  
+    int N, M; // Number of buckets and entries in those buckets 
+    Node heads; // Nodes at the start of the buckets
+    
+    Node Search(
